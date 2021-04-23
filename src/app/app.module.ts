17d24b3e35a5fb372ae5c7fa17e19e1073672ee5
Marcos <marcoslsouzadf@gmail.com>
2021-04-23 01:19:20 -0300

    Criacao do componente foto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FotoComponent } from './foto/foto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FotoComponent
   ],
   imports: [
     BrowserModule
