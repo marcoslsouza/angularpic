32d68559b2057920bb5e72d85e4f1f780b1b6938
Marcos <marcoslsouzadf@gmail.com>
2021-04-24 19:24:26 -0300

    Criacao do modulo fotos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FotoComponent } from './foto/foto.component';
+import { FotosModule } from './fotos/fotos.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FotoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
