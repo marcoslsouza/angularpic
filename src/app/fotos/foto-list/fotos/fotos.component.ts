e8338d469f181d7ce6db732bf038c9ed30f8d013
Marcos <marcoslsouzadf@gmail.com>
2021-05-04 03:01:25 -0300

    Ajuste de layout apresentando de 3 em 3 fotos por linha

--- src/app/fotos/foto-list/fotos/fotos.component.ts
+++ src/app/fotos/foto-list/fotos/fotos.component.ts
@@ -1,19 +1,33 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Foto } from '../../foto/foto';
 
 @Component({
     selector: 'ap-fotos',
     templateUrl: 'fotos.component.html'
 })
-export class FotosComponent implements OnInit {
+export class FotosComponent implements OnChanges {
   
   // Array de Object
  @Input() fotos: Foto[] = [];
 
+ rows: any[] = [];
+
   constructor() {
   }
 
-  ngOnInit(): void {
-    
+  ngOnChanges(changes: SimpleChanges) {
+      if(changes.fotos) {
+          this.rows = this.groupColumns(this.fotos);
+      }
+  }
+
+  groupColumns(fotos: Foto[]) {
+    const newRows = [];
+
+    for(let index = 0; index < fotos.length; index+=3) {
+      newRows.push(fotos.slice(index, index + 3));
+    }
+
+    return newRows;
   }
 }
