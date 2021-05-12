816eb2335300145523db4fb5c491eddd81aac3eb
Marcos <marcoslsouzadf@gmail.com>
2021-05-12 14:06:41 -0300

    Implementacao do debounceTime para que o filtro de fotos seja executado ao pausar 3 milesegundos no input de pesquisa

--- src/app/fotos/foto-list/foto-list.component.ts
+++ src/app/fotos/foto-list/foto-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { Foto } from '../foto/foto';
 
 @Component({
@@ -10,11 +12,14 @@ export class FotoListComponent implements OnInit {
   // Array de Object
   fotos: Foto[] = [];
   filtro: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     // Recupera o valor de FotoListResolver
     this.fotos = this.activatedRoute.snapshot.data.fotos;
+
+    this.debounce.pipe(debounceTime(300)).subscribe(filtro => this.filtro = filtro);
   }
 }
